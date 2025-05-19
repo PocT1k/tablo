740f721e521b3e065beb315d01634b7308a07760
Kirill Novikov <novikovk2003@yandex.ru>
2025-05-19 18:22:34 +0300

    0.5.5 переименование файлов логирования

--- script/stats.py
+++ script/stats.py
@@ -1,10 +1,39 @@
-
-
+from builtins import enumerate
+import os
 from conf import (
-    FACE_ATTENDANCE_PATH, FACE_ATTENDANCE_OLD_DIR,
-    VOSK_WORLD_PATH, VOSK_WORLD_OLD_DIR,
-    YAMNET_INDICES_PATH,YAMNET_INDICES_OLD_DIR,
-    YOLO_CLASSEC_PATH, YOLO_CLASSEC_OLD_DIR
+    FACE_ATTENDANCE_FILE, FACE_ATTENDANCE_OLD,
+    VOSK_WORLD_FILE, VOSK_WORLD_OLD,
+    YAMNET_INDICES_FILE,YAMNET_INDICES_OLD,
+    YOLO_CLASSEC_FILE, YOLO_CLASSEC_OLD
 )
-from load import get_dated_path
+from load import get_log_path
 
+
+LOGS_FILES = [FACE_ATTENDANCE_FILE, VOSK_WORLD_FILE, YAMNET_INDICES_FILE, YOLO_CLASSEC_FILE]
+LOGS_OLDS = [FACE_ATTENDANCE_OLD, VOSK_WORLD_OLD, YAMNET_INDICES_OLD, YOLO_CLASSEC_OLD]
+TIME_REC = ["image_time_recognition", "image_time_recognition", "audio_time_recognition", "audio_time_recognition"]
+
+
+def get_stats(setting, stat_name, stat_period):
+    cont_logs = len(LOGS_FILES)
+    if cont_logs != len(LOGS_OLDS) or cont_logs != len(TIME_REC):
+        raise 'Ошибка stats.py - неверно указанны пути логирования, неправильначя длинна'
+
+    # Определения путей и существования файлов
+    paths = []
+    for i, file in enumerate(LOGS_FILES):
+        path, exists = get_log_path(file, LOGS_OLDS[i], stat_period[0])
+        paths.append(exists)
+
+    # Определение % собранной информации - будет в выводе
+    for i in range (len(paths)):
+        exists = os.path.exists(paths[i])
+
+    # Анализ каджого файла отдельно
+
+    # Сбор общего анализа
+
+    # Итогова оценка - главный вывод
+
+
+#TODO - весь анализ статистики
